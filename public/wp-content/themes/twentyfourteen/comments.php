09d6587911a312077a8305de052bb2d10ab8ecdf
fernandofmendes <fernando.mendes@webca.com.br>
2016-06-09 17:24:10 -0300

    adicionado comentario

--- public/wp-content/themes/twentyfourteen/comments.php
+++ public/wp-content/themes/twentyfourteen/comments.php
@@ -12,6 +12,7 @@
 /*
  * If the current post is protected by a password and the visitor has not yet
  * entered the password we will return early without loading the comments.
+ dwqdwdwdw
  */
 if ( post_password_required() ) {
 	return;
